e4335545adb1e16a4669d42b9c49bdd8a0329d64
bethmannRolf <devak.robe@gmail.com>
2024-08-31 17:51:25 +0200

    imtegrated level class

--- models/world.class.js
+++ models/world.class.js
@@ -2,26 +2,13 @@ class World {
 
 
     character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-
-    ];
-    clouds = [
-        new Cloud()
-    ];
-    backgroundObjects = [
-
-
-        new BackgroundObject('img/5_background/layers/air.png', 0),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0)
-    ];
+    enemies = level1.enemies;
+    clouds = level1.clouds ;
+    backgroundObjects = level1.backgroundObjects;
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
@@ -38,14 +25,13 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+        this.ctx.translate(this.camera_x, 0);
+
         this.addObjectsToMap(this.backgroundObjects);
         this.addObjectsToMap(this.clouds);
-
-
         this.addObjectsToMap(this.enemies);
         this.addToMap(this.character);
-
-
+        this.ctx.translate(-this.camera_x, 0);
 
         let self = this;
         requestAnimationFrame(function () {
