43df7fe08343f744614a8d36b6d8fec8c6475f82
bethmannRolf <devak.robe@gmail.com>
2024-09-02 15:18:32 +0200

    integrated frames for collision

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -8,8 +8,27 @@ class MovableObject {
     currentImage = 0;
     speed = 0.15;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 1;
 
-    //loadImage(''img/test.png')
+
+    applyGravity() {
+        setInterval(() => {
+            if(this.isAboveGround() || this.speedY > 0){
+            this.y  -= this.speedY;
+            this.speedY -= this.acceleration;
+        }
+        }, 1000 / 25);
+    }
+
+
+    isAboveGround(){
+
+        return this.y < 180;
+    }
+
+
+
     loadImage(path) {
         this.img = new Image()
         this.img.src = path;
@@ -17,17 +36,36 @@ class MovableObject {
     }
 
     loadImages(arr) {
- 
-        arr.forEach(path =>{
+
+        arr.forEach(path => {
 
             let img = new Image();
             img.src = path;
-            img.style ='transform: scaleX(-1)'
+            img.style = 'transform: scaleX(-1)'
             this.imageCache[path] = img;
         });
     }
 
-    playAnimation(images){
+    draw(ctx){
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+    drawFrame(ctx){
+
+if (this instanceof Character || this instanceof Chicken || this instanceof Endboss) {
+    
+
+
+
+
+       ctx.beginPath();
+       ctx.lineWidth = '5';
+      ctx.strokeStyle = "blue";
+      ctx.rect(this.x, this.y, this.width, this.height);
+       ctx.stroke();
+}
+    }
+
+    playAnimation(images) {
         let i = this.currentImage % this.IMAGES_WALKING.length;
         let path = images[i];
         this.img = this.imageCache[path];
@@ -35,15 +73,23 @@ class MovableObject {
     }
 
 
+    moveRight(){
+        this.x += this.speed;
+     
+    }
 
+    moveLeft() {
 
-    moveLeft(){
-        setInterval(() => {
-            this.x -= this.speed;
-        }, 1000/60
-        )
+       
+            this.x -= this.speed
+          
 
     }
+
+jump(){
+    this.speedY = 20;
+}
+
 
 
 }
