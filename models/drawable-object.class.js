397d6aaf5b87723d6e2733f2865b988f872bfa06
bethmannRolf <devak.robe@gmail.com>
2024-09-03 19:05:20 +0200

    integrated drawable object

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -1,7 +1,33 @@
 class DrawableObject {
 
+    x = 120;
+    y = 280;
+    height = 150;
+    width = 100;
+
+    img;
+    imageCache = {};
+    currentImage = 0;
 
 
+    loadImage(path) {
+        this.img = new Image()
+        this.img.src = path;
+    }
 
-    
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    loadImages(arr) {
+
+        arr.forEach(path => {
+
+            let img = new Image();
+            img.src = path;
+            img.style = 'transform: scaleX(-1)'
+            this.imageCache[path] = img;
+        });
+    }
+
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,9 @@
-class MovableObject {
-    x = 120;
-    y = 280;
-    height = 150;
-    width = 100;
-    img;
-    imageCache = [];
-    currentImage = 0;
+class MovableObject extends DrawableObject {
+
+ 
+  
+
+  
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
@@ -47,25 +45,13 @@ class MovableObject {
         this.energy == 0;
     }
 
-    loadImage(path) {
-        this.img = new Image()
-        this.img.src = path;
-    }
 
-    loadImages(arr) {
 
-        arr.forEach(path => {
 
-            let img = new Image();
-            img.src = path;
-            img.style = 'transform: scaleX(-1)'
-            this.imageCache[path] = img;
-        });
-    }
 
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
+
+
+
     drawFrame(ctx) {
 
         if (this instanceof Character || this instanceof Chicken || this instanceof Endboss) {
