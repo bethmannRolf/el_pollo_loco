4bc16d8e7c8216ddb32331e3525404acdd0a2782
bethmannRolf <devak.robe@gmail.com>
2024-09-10 18:55:20 +0200

    integrated bottle animation before collecting

--- models/collectable-bottle-object.class.js
+++ models/collectable-bottle-object.class.js
@@ -1,18 +1,32 @@
 class CollectableBottleObject extends MovableObject{
 
 
+IMAGES_BOTTLE_ALTERNATING = [
+    'img/6_salsa_bottle/1_salsa_bottle_on_ground.png',
+    'img/6_salsa_bottle/2_salsa_bottle_on_ground.png'
+]
+
+
+
     constructor(){
 
-        super().loadImage('img/6_salsa_bottle/1_salsa_bottle_on_ground.png')
+        super().loadImage(this.IMAGES_BOTTLE_ALTERNATING[0]);
+        this.loadImages(this.IMAGES_BOTTLE_ALTERNATING)
         this.x = 300;
         this.y = 300;
         this.width = 80
         this.height = 80
       
-
+        this.animate()
 
     }
 
+    animate() {
+
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_BOTTLE_ALTERNATING)
+        }, 350)
+    }
 
 
 }
